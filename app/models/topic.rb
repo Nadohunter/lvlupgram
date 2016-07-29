d81733028022e223e46e09d49321ab22162da2d8
nadohunter <nadohunter@gmail.com>
2016-07-29 18:27:26 +0000

    paperclip

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -3,8 +3,9 @@ class Topic < ActiveRecord::Base
     
     
     has_attached_file :image, styles: {
-     :medium => '640x',
-     :small =>'400x'
+     thumb: '100x100>',    
+     medium: '640x',
+     small: '400x'
      }
     
     validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
