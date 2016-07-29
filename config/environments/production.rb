43fa8b92cea2e6f688c3d492315b6984769e4f0d
nadohunter <nadohunter@gmail.com>
2016-07-29 18:43:10 +0000

    paperclip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
   
   
   
-  config.paperclip_defaults = {
+   config.paperclip_defaults = {
   storage: :s3,
    s3_credentials: {
      bucket: ENV.fetch('S3_BUCKET_NAME'),
