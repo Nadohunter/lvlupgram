790e70dbca67bb6db03ff6774666d8d36ab49826
nadohunter <nadohunter@gmail.com>
2016-07-29 18:00:09 +0000

    paperclip

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,12 +2,12 @@ class Topic < ActiveRecord::Base
     has_many :votes, dependent: :destroy
     
     
-has_attached_file :image, styles: {
+    has_attached_file :image, styles: {
      :medium => '640x',
      :small =>'400x'
      }
     
-validates_attachment_content_type :image, :content_type => :image, :content_type => /\Aimage\/.*\Z/
+    validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
  
 
 end
