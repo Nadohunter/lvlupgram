d772800987ab2e732038a705051ed029818cb81d
nadohunter <nadohunter@gmail.com>
2016-07-29 18:32:32 +0000

    paperclip

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,13 +1,14 @@
 class Topic < ActiveRecord::Base
-    has_many :votes, dependent: :destroy
-    
-    
+     has_many :votes, dependent: :destroy
     has_attached_file :image, styles: {
-     thumb: '100x100>',    
-     medium: '640x',
-     small: '400x'
-     }
-    
+      thumb: '100x100>',
+      square: '200x200#',
+      medium: '300x300>'
+     } 
+     #:url => '/public/:attachment/000/000/:id/:style_:basename.:extension'
+
+
+    # Validate the attached image is image/jpg, image/png, etc
     validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
  
 
